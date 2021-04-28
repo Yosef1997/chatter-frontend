c2ccb47428af14b7c528c7e804425256436fa963
unknown <yosefsitumorang97@gmail.com>
2021-04-28 22:06:15 +0700

    header home integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,39 +7,39 @@ import ProfileImg from '../assets/F9.jpg';
 import {connect} from 'react-redux';
 import {detailUser} from '../components/Redux/Action/auth';
 import {detailChatUser} from '../components/Redux/Action/user';
-// import {REACT_APP_API_URL as API_URL} from '@env';
+import {REACT_APP_API_URL as API_URL} from '@env';
 
 class Home extends Component {
   render() {
     return (
       <ScrollView style={styles.backImgage}>
-        {/* {this.props.auth.detailUser.picture === null ? ( */}
-        <CardProfile
-          source={ProfileImg}
-          label="Yosef"
-          // message={this.props.auth.detailUser.status}
-          icon2="settings-outline"
-          size={25}
-          parent={styles.card}
-          image={styles.cardImg}
-          onPress={() => this.props.navigation.navigate('Profil')}
-        />
-        {/* ) : (
-        <CardProfile
-            source={{
-              uri: API_URL.concat(
-                `/upload/profile/${this.props.auth.detailUser.picture}`,
-              ),
-            }}
-            label={this.props.auth.detailUser.name}
-            message={this.props.auth.detailUser.status}
+        {this.props.auth.user.picture === null ? (
+          <CardProfile
+            source={ProfileImg}
+            label="Yosef"
+            message={this.props.auth.user.status}
             icon2="settings-outline"
             size={25}
-            style={styles.card}
+            parent={styles.card}
             image={styles.cardImg}
             onPress={() => this.props.navigation.navigate('Profil')}
           />
-        )} */}
+        ) : (
+          <CardProfile
+            source={{
+              uri: API_URL.concat(
+                `/upload/profile/${this.props.auth.user.picture}`,
+              ),
+            }}
+            label={this.props.auth.user.name}
+            message={this.props.auth.user.status}
+            icon2="settings-outline"
+            size={25}
+            parent={styles.card}
+            image={styles.cardImg}
+            onPress={() => this.props.navigation.navigate('Profil')}
+          />
+        )}
         <TouchableOpacity
           onPress={() => this.props.navigation.navigate('Search')}>
           <Search
